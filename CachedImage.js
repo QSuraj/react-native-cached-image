b6da93746aaab4586f93e769682dfb7befaa2c42
Suraj M Durgad <suraj@quizizz.com>
2023-08-25 16:31:42 +0530

    CachecImage using uriblob instead of fetch

--- CachedImage.js
+++ CachedImage.js
@@ -168,11 +168,43 @@ class CachedImage extends React.Component {
             });
     }
 
+  /**
+   * Function to convert a URI to a Blob object
+   * @param {string} uri - The URI of the file
+   * @returns {Promise} - Returns a promise that resolves with the Blob object
+   */
+  uriToBlob(uri){
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    // If successful -> return with blob
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    // reject on error
+    xhr.onerror = function () {
+      reject(new Error('uriToBlob failed'));
+    };
+
+    // Set the response type to 'blob' - this means the server's response
+    // will be accessed as a binary object
+    xhr.responseType = 'blob';
+
+    // Initialize the request. The third argument set to 'true' denotes
+    // that the request is asynchronous
+    xhr.open('GET', uri, true);
+
+    // Send the request. The 'null' argument means that no body content is given for the request
+    xhr.send(null);
+  });
+};
+
     async readXmlFromFile(){
       const source = (this.state.isCacheable && this.state.cachedImagePath) ? {
         uri: 'file://' + this.state.cachedImagePath
     } : this.props.source;
-      await fetch(source.uri)
+      await uriToBlob(source.uri)
       .then((response) => response.text())
       .then((xml) =>{
         this.setState({xml})
